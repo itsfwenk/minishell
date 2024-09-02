856d377833ca462a0c0fb66be4592260f367bd5a
Franck Li <fli@made-f0Dr9s12.clusters.42paris.fr>
2024-09-02 19:40:08 +0200

    expander in progress (beginning)

--- parser_in_progress/expander/ft_expander.c
+++ parser_in_progress/expander/ft_expander.c
@@ -6,9 +6,52 @@
 /*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:09:48 by fli               #+#    #+#             */
-/*   Updated: 2024/09/02 17:10:54 by fli              ###   ########.fr       */
+/*   Updated: 2024/09/02 19:39:58 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static int	exp_check_key(char *key)
+{
+	size_t	i;
+
+	i = 0;
+	while (key[i] != ' ')
+	{
+		if (!i && !ft_isalpha(key[i]) && key[i] != '_')
+			return (0);
+		if (i && !ft_isalnum(key[i]) && key[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	expand_env_var(t_string *current)
+{
+	int	i;
+
+	i = 0;
+	if (current->to_be_expanded == TRUE)
+	{
+		while (current->str[i] != '\0')
+		{
+			if (current->str[i] == '$')
+			{
+
+			}
+		}
+	}
+}
+
+void	ft_expander(t_string *tstring)
+{
+	t_string	*current;
+
+	current = tstring;
+	while (current != NULL)
+	{
+
+	}
+}
