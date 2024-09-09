21c01465e6666d90e8973344f368a4dc5598b633
Franck Li <fli@made-f0Dr10s13.clusters.42paris.fr>
2024-09-09 19:49:54 +0200

    in wildcards, check filename

---
+++ parser_in_progress/expander/assemble_tstring.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   assemble_tstring.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 13:42:41 by fli               #+#    #+#             */
+/*   Updated: 2024/09/09 14:33:18 by fli              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	get_full_len(t_token *current)
+{
+	int			full_len;
+	t_string	*tstring;
+
+	full_len = 0;
+	tstring = current->tstring;
+	while (tstring != NULL)
+	{
+		full_len = full_len + ft_strlen(tstring->str);
+		tstring = tstring->next;
+	}
+	return (full_len);
+}
+
+void	cat_tstring(t_token *current, char *assembled, int	*i)
+{
+	int			j;
+	t_string	*current_tstring;
+
+	current_tstring = current->tstring;
+	while (current_tstring != NULL)
+	{
+		j = 0;
+		while (current->tstring->str[j] != '\0')
+		{
+			assembled[*i] = current->tstring->str[j];
+			(*i)++;
+			j++;
+		}
+		current_tstring = current_tstring->next;
+	}
+	assembled[*i] = '\0';
+}
+
+int	assemble_tstring(t_token *tokens)
+{
+	int		i;
+	char	*assembled;
+	t_token	*current;
+
+	current = tokens;
+	while (tokens != NULL)
+	{
+		i = 0;
+		assembled = malloc((get_full_len(current) + 1) * sizeof(char));
+		if (assembled == NULL)
+		{
+			return (0); //ft_clean exit
+		}
+		cat_tstring(current, assembled, &i);
+		tstring_del(current->tstring->next);
+		free(current->tstring->str);
+		current->tstring->str = assembled;
+		current = current->next;
+	}
+}
--- parser_in_progress/expander/ft_wildcards.c
+++ parser_in_progress/expander/ft_wildcards.c
@@ -6,22 +6,60 @@
 /*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:27:45 by fli               #+#    #+#             */
-/*   Updated: 2024/09/06 18:57:04 by fli              ###   ########.fr       */
+/*   Updated: 2024/09/09 19:39:44 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	check_filename(t_string *current, struct dirent *read_return, int *i, int *j)
+{
+	int	k;
+
+	k = 0;
+	while (current->str[*i] != '\0')
+	{
+		if (current->str[*i + *j] == '*')
+		{
+			i++;
+			if (current->str[*i + *j] == '\0')
+				return (TRUE);
+			while (current->str[*i] != read_return->d_name[*j])
+			{
+				if (current->str[*i + *j] == '*')
+					check_filename(current, read_return, i, j);
+				else
+					j++;
+			}
+		}
+		while (read_return->d_name[*j + k] != '\0' && current->str[*i + k] != '\0')
+		{
+			if (current->str[*i + k] == '*')
+				check_filename(current, read_return, i, j);
+			else if (current->str[*i + *j] != read_return->d_name[*j])
+			{
+				k = 0;
+				*j++;
+				continue ;
+			}
+			else
+				*k++;
+		}
+	}
+	return (FALSE);
+}
+
 char	*get_filenames(t_string *current)
 {
-	char	*filenames;
-	DIR		*directory;
-	struct	dirent *read_return;
+	t_string		*filenames;
+	DIR				*directory;
+	struct	dirent	*read_return;
 
 	directory = opendir(".");
 	read_return = readdir(directory);
 	while (read_return != NULL)
 	{
+
 		printf("%s\n", read_return->d_name);
 		read_return = readdir(directory);
 	}
