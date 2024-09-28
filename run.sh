9a9f8bc5be7cff8155914442ca5611e3de846de2
Mathis El Habib <mel-habi@made-f0Br6s2.clusters.42paris.fr>
2024-09-28 16:40:07 +0200

    Del

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir=$(pwd)
-
-valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all --trace-children=yes --show-reachable=yes --suppressions=$dir/readline.supp ./minishell
