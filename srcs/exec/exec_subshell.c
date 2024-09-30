7de58a8c44caedc58dd2b3c7c34936402bb56898
Mathis El Habib <mel-habi@made-f0Br7s1.clusters.42paris.fr>
2024-09-30 18:11:46 +0200

    Fix still reachables

--- srcs/exec/exec_subshell.c
+++ srcs/exec/exec_subshell.c
@@ -6,7 +6,7 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:26:03 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/09/29 10:32:50 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/09/30 18:11:03 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ static void	subshell_child_exec(t_skibidi *shell, t_token *tree,
 	t_token	*par_tree;
 	t_token	*sub_token;
 
-	dup2(pipetab[0], STDIN_FILENO);
+	if (pipetab)
+		dup2(pipetab[0], STDIN_FILENO);
 	close_pipe(pipetab);
 	par_tree = create_tree(tree->sub_shell);
 	if (exec_tree(shell, par_tree, NULL, side) == true)
@@ -27,10 +28,15 @@ static void	subshell_child_exec(t_skibidi *shell, t_token *tree,
 		while (sub_token)
 		{
 			if (sub_token->type == STR)
-				waitpid(sub_token->pid->p_id,
-					&sub_token->pid->status, 0);
+			{
+				waitpid(sub_token->pid->p_id, &sub_token->pid->status, 0);
+				update_error_code(shell, sub_token->pid->status);
+			}
 			sub_token = sub_token->next;
 		}
+		lx_deltokens(&shell->tokens);
+		free_env(shell->env);
+		free(shell);
 		exit(EXIT_SUCCESS);
 	}
 }
@@ -38,9 +44,6 @@ static void	subshell_child_exec(t_skibidi *shell, t_token *tree,
 void	exec_parentheses(t_skibidi *shell, t_token *tree, int *pipetab,
 	t_side side)
 {
-	tree->pid = ft_lstnew_pipex(shell);
-	if (tree->pid == NULL)
-		exit_shell(shell);
 	tree->pid->p_id = fork();
 	if (tree->pid->p_id == -1)
 		exit_shell(shell);
