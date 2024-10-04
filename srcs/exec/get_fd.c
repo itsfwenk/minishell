f676b00a4b72558c4f5c63dc5fa536876b7f8efd
Franck Li <fli@made-f0Br7s2.clusters.42paris.fr>
2024-10-04 17:23:17 +0200

    fix no file after redir

--- srcs/exec/get_fd.c
+++ srcs/exec/get_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_fd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:25:21 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/09/29 12:05:49 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/04 17:21:57 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,14 @@ int	get_fd(t_skibidi *shell, t_token *redirection)
 	int		fd_redir;
 
 	fd_redir = -1;
-	if (redirection->type == IN_REDIR)
+	if (redirection->type == IN_REDIR && redirection->next)
 		fd_redir = open(redirection->next->assembled, O_RDONLY);
-	if (redirection->type == HERE_DOC)
+	if (redirection->type == HERE_DOC && redirection->next)
 		fd_redir = get_hd_fd(shell, redirection);
-	if (redirection->type == OUT_REDIR)
+	if (redirection->type == OUT_REDIR && redirection->next)
 		fd_redir = open(redirection->next->assembled,
 				O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (redirection->type == APD_OUT_REDIR)
+	if (redirection->type == APD_OUT_REDIR && redirection->next)
 		fd_redir = open(redirection->next->assembled,
 				O_WRONLY | O_CREAT | O_APPEND, 0644);
 	redirection->fd = fd_redir;
