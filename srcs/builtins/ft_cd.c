6b3be2d776f888707b3394c5c35dca5eed7b4aa8
mel-habi <mel-habi@student.42.fr>
2024-10-06 01:50:21 +0200

    Refix cd behavior

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 19:05:28 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/06 01:28:03 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/06 01:49:59 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,31 @@ static int	try_cd_home(t_env *env)
 	return (1);
 }
 
+static void	export_pwd(t_env *env, char *nwd)
+{
+	t_env	*pwd;
+	t_env	*oldpwd;
+
+	pwd = get_env(env, "PWD");
+	oldpwd = get_env(env, "OLDPWD");
+	if (pwd && pwd->is_exported && !pwd->is_unset
+		&& oldpwd && oldpwd->is_exported && !oldpwd->is_unset)
+		add_env(&env, "OLDPWD", pwd->value);
+	else if (!pwd || !pwd->is_exported || pwd->is_unset)
+	{
+		unset_env(env, "OLDPWD");
+		unset_env(env, "PWD");
+	}
+	else
+	{
+		add_env(&env, "OLDPWD", pwd->value);
+		add_env(&env, "PWD", nwd);
+	}
+}
+
 int	ft_cd(t_env	*env, char **args)
 {
 	char	nwd[PATH_MAX];
-	t_env	*pwd;
 
 	if (!args[0] || (!ft_strcmp(args[0], "~") && !args[1]))
 		return (try_cd_home(env));
@@ -48,11 +69,6 @@ int	ft_cd(t_env	*env, char **args)
 		ft_print_error("cd", args[0], strerror(errno), "\0");
 		return (1);
 	}
-	pwd = get_env(env, "PWD");
-	if (pwd && pwd->is_exported && !pwd->is_unset)
-		add_env(&env, "OLDPWD", pwd->value);
-	else
-		return (unset_env(env, "OLDPWD"), unset_env(env, "PWD"), 0);
-	add_env(&env, "PWD", nwd);
+	export_pwd(env, nwd);
 	return (0);
 }
