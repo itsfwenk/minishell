e6c7c18b2d2e72cdcc1dde2c4a76cfdb088659ed
Mathis El Habib <mel-habi@made-f0Br5s1.clusters.42paris.fr>
2024-10-07 16:48:05 +0200

    Fix heredoc limiter

--- srcs/exec/here_doc_utils.c
+++ srcs/exec/here_doc_utils.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 19:17:15 by fli               #+#    #+#             */
-/*   Updated: 2024/10/03 19:17:41 by fli              ###   ########.fr       */
+/*   Updated: 2024/10/07 16:47:10 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "skibidishell.h"
+
+static void	cp_limiter(char	*limiter, t_token *limiter_token)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (limiter_token->full_string[i] != '\0')
+	{
+		if (in_charset(limiter_token->full_string[i], "'\"") == false)
+		{
+			limiter[j] = limiter_token->full_string[i];
+			j++;
+		}
+		i++;
+	}
+	limiter[j] = '\0';
+}
 
 char	*get_limiter(t_skibidi *shell, t_token *limiter_token)
 {
@@ -29,13 +48,6 @@ char	*get_limiter(t_skibidi *shell, t_token *limiter_token)
 	limiter = ft_calloc(len + 1, sizeof(char));
 	if (limiter == NULL)
 		exit_shell(shell);
-	i = 0;
-	while (limiter_token->full_string[i] != '\0')
-	{
-		if (in_charset(limiter_token->full_string[i], "'\"") == false)
-			limiter[i] = limiter_token->full_string[i];
-		i++;
-	}
-	limiter[i] = '\0';
+	cp_limiter(limiter, limiter_token);
 	return (limiter);
 }
